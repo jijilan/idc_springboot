5de78408725a237a9b3330fed54bbdd1e7afdbdd
yangqc <374866976@qq.com>
2020-11-27 19:00:04 +0800

    根据品牌id,获取产品列表信息

--- src/main/java/com/idc/modules/controller/agent/BrandSummaryController.java
+++ src/main/java/com/idc/modules/controller/agent/BrandSummaryController.java
@@ -1,9 +1,28 @@
 package com.idc.modules.controller.agent;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.idc.common.result.ResultView;
+import com.idc.common.utils.EmptyUtil;
+import com.idc.modules.entity.BrandBasicInfor;
+import com.idc.modules.entity.BrandDictionary;
+import com.idc.modules.entity.BrandPerson;
+import com.idc.modules.service.IBrandBasicInforService;
+import com.idc.modules.service.IBrandDictionaryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -13,8 +32,41 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Dylan
  * @since 2020-11-27
  */
-@RestController
-@RequestMapping("/brand-summary")
+@Slf4j
+@RestController("BrandSummary")
+@RequestMapping("/api/brand-summary/agent")
+@Validated
 public class BrandSummaryController {
+    @Autowired
+    private IBrandDictionaryService iBrandDictionaryService;
+    @Autowired
+    private IBrandBasicInforService iBrandBasicInforService;
+
+    // 保存品牌基础信息: 基础信息+产品信息
+    @PostMapping(value = "/saveBrandSummary")
+    public ResultView saveBrandSummary(@RequestBody JSONObject jsonParam) {
+        String brandSummaryStr=jsonParam.getString("basinfor");
+        String summaryProductStr=jsonParam.getString("brandPersons");
+        String brandIdStr=jsonParam.getString("brandId");
+        return ResultView.ok();
+    }
+
+    @PostMapping(value = "/getProductListByBrandId")
+    public ResultView getProductListByBrandId(@NotNull(message = "品牌id不能为空") int brandId) {
+        // 1.获取品牌信息中配置的产品
+        QueryWrapper<BrandBasicInfor> basWrapper=new QueryWrapper<>();
+        basWrapper.lambda().eq(BrandBasicInfor::getId,brandId);
+        BrandBasicInfor basicInfor=iBrandBasicInforService.getOne(basWrapper);
+        QueryWrapper<BrandDictionary> proWrapper=new QueryWrapper<>();
+        proWrapper.lambda().in(BrandDictionary::getCode,basicInfor.getCailliaolb().split(",")).orderByDesc(BrandDictionary::getBySort);
+        List<Map<String,Object>> productInforList= iBrandDictionaryService.listMaps(proWrapper.lambda().select(BrandDictionary::getCode,BrandDictionary::getName,BrandDictionary::getRemark));
+        if(EmptyUtil.isEmpty(productInforList)){
+            return ResultView.error("产品列表为空");
+        }
+        return ResultView.ok(productInforList);
+    }
+
+
+
 
 }
